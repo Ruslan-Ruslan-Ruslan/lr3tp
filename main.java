0cef336736e8921f497e50842d595eb0ef7d8553
Екатерина Хазнаферова <lashkoruslan@gmail.com>
2024-10-28 00:27:15 +0200

    Доданий код

--- main.java
+++ main.java
@@ -1,3 +1,24 @@
-public class Main {
-    
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Запит користувача на введення речення
+        System.out.println("Введіть речення: ");
+        String sentence = scanner.nextLine();
+
+        // Перевірка типу речення
+        if (sentence.endsWith("!")) {
+            System.out.println("Це окличне речення.");
+        } else if (sentence.endsWith("?")) {
+            System.out.println("Це питальне речення.");
+        } else {
+            System.out.println("Це звичайне речення.");
+        }
+        scanner.close();
+    }
 }
+
+
+
